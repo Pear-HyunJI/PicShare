a166f90a1aad59f3ddc05249de14395766640222
joyoung-e <joyoung_e@naver.com>
2024-06-07 18:22:09 +0900

    댓글기능추가

--- client/src/components/list/CommentList.jsx
+++ client/src/components/list/CommentList.jsx
@@ -22,18 +22,18 @@ const CommentListBlock = styled.div`
     border-radius: 5px;
   }
 `;
-const LikeItem = styled.div`
+const commentItem = styled.div`
   margin-bottom: 10px;
   border: 1px solid #ddd;
   border-radius: 5px;
   padding: 10px;
-  .like {
+  .comment {
     color: #000;
     z-index: 9999;
     font-size: 25px;
     margin-bottom: 10px;
   }
-  .likeImg {
+  .commentImg {
     width: 10%;
     margin-bottom: 10px;
   }
@@ -41,29 +41,29 @@ const LikeItem = styled.div`
 
 const CommentList = () => {
   const dispatch = useDispatch();
-  const [likeList, setLikeList] = useState([]);
+  const [commentList, setCommentList] = useState([]);
   const user = useSelector((state) => state.members.user);
 
   useEffect(() => {
     if (user) {
       axios
-        .post(`${serverUrl}/other/post/likeList`, {
+        .post(`${serverUrl}/other/post/commentList`, {
           userNo: user.userNo,
         })
         .then((res) => {
           if (res.data) {
-            setLikeList(res.data);
+            setCommentList(res.data);
           } else {
-            console.log("좋아요 데이터를 가져오는데 실패했습니다.");
+            console.log("댓글 데이터를 가져오는데 실패했습니다.");
           }
         })
         .catch((error) => {
-          console.error("좋아요 데이터를 가져오는 중 오류 발생:", error);
+          console.error("댓글 데이터를 가져오는 중 오류 발생:", error);
         });
     }
   }, [dispatch, user]);
 
-  console.log("라이크리스트", likeList);
+  console.log("코멘트리스트", commentList);
 
   return (
     <CommentListBlock>
@@ -72,18 +72,18 @@ const CommentList = () => {
         &nbsp;&nbsp;내가 작성한 댓글 목록
       </h2>
       <div>
-        {likeList.map((like) => (
-          <LikeItem key={like.postId}>
-            <div className="like">
+        {commentList.map((comment) => (
+          <CommentItem key={comment.postId}>
+            <div className="comment">
               <AiFillMessage />
             </div>
             <div></div>
 
-            <div className="likeImg">
-              <img src={`${serverUrl}/uploads/${like.imageUrls}`} alt="Post" />
+            <div className="commentImg">
+              <img src={`${serverUrl}/uploads/${comment.imageUrls}`} alt="Post" />
             </div>
-            <p className="likeContent">게시글 : {like.content}</p>
-          </LikeItem>
+            <p className="commentContent">게시글 : {comment.content}</p>
+          </CommentItem>
         ))}
       </div>
     </CommentListBlock>
